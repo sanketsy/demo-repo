293f7881897aa63b47177dc826a4bb6c6edaa55e
sanketsy <yelkarsanket@gmail.com>
2020-07-30 00:15:31 +0530

    Modified new.java

--- new.java
+++ new.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-    System.out.println("Hello");
+class ntew{
+        public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
